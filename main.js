bc3b84d324798c8992caebde891f6e8e70ea6079
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-01-23 15:42:48 +0900

    22222

--- main.js
+++ main.js
@@ -9,6 +9,33 @@ var app = http.createServer(function(request,response){
     var title = queryData.id;
  
     if(pathname === '/'){
+        
+        if(queryData.id === undefined){
+            var title = "Welcome";
+            var description = 'Hello';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JS">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+        response.writeHead(200);
+        response.end(template);
+            
+      }else{
       fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
         var template = `
         <!doctype html>
@@ -32,12 +59,10 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
-    } else {
+      }
+    }else {
       response.writeHead(404);
       response.end('Not found');
-    }
- 
- 
- 
+    } 
 });
 app.listen(3000);
