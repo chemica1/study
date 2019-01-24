6c0940021e3ec2d69a10a51297b2d00692cadd75
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-01-24 17:26:16 +0900

    스파게티 맛있겠다...
    점점 요리가 되어가는중

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var querystring = require('querystring')
  
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -45,7 +45,7 @@ var app = http.createServer(function(request,response){
               var title = 'Welcome';
               var description = 'Hello, Node.js';
               var list = templateList(filelist);
-              var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+              var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
               response.writeHead(200);
               response.end(template);
             })
@@ -54,7 +54,8 @@ var app = http.createServer(function(request,response){
               fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
                 var list = templateList(filelist);
-                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a>
+    <a href="/update">update</a>`);
                 response.writeHead(200);
                 response.end(template);
               });
@@ -84,7 +85,7 @@ var app = http.createServer(function(request,response){
             <h2>${title}</h2>
             ${description}
           
-            `);
+            `,``);
           response.writeHead(200);
           response.end(template);
         })
