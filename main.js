588f9bb910d35a44ef85e95fe267c5e703b5491e
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-01-25 16:35:51 +0900

    삭제버튼을 구현했지만...
    update부분에 오류가 생겼다
    value에 undefined가 들어가있는게 문제.
    왜지??

--- main.js
+++ main.js
@@ -48,13 +48,19 @@ var app = http.createServer(function(request,response){
               response.writeHead(200);
               response.end(template);
             })
-          }else{
+          }else{ //컨텐츠를 선택했을때 나오는 부분
             fs.readdir('./data', function(error, filelist){
               fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
                 var list = templateList(filelist);
-                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a>
-                <a href="/update?id=${title}">update</a>`);
+                var template = templateHTML(title, list,
+                                            `<h2>${title}</h2>${description}`,
+                                            `<a href="/create">create</a>
+                                            <a href="/update">update</a>
+                                            <form action="delete_process" method="post">
+                                            <input type="hidden" name="id" value="${title}">
+                                            <input type="submit" value="delete">
+                                            </form>`);
                 response.writeHead(200);
                 response.end(template);
               });
@@ -152,6 +158,20 @@ var app = http.createServer(function(request,response){
                    response.end();
                 });
             });
+        });
+    }else if(pathname === '/delete_process'){
+        var body = '';
+        request.on('data', function(data){
+              body = body + data;
+        });
+        request.on('end', function(){
+              var post = querystring.parse(body);
+              var id = post.id;
+             fs.unlink(`data/${id}`, function(error){
+                       response.writeHead(302, {Location: `/`});
+                   response.end();
+             });
+  
         });
     }else{
       response.writeHead(404);
